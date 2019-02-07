166c108941d67ed8ac0625892911b6d707af61f7
Guille <100346149@alumnos.uc3m.es>
2019-02-07 11:42:07 +0100

    Versión final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'microp1';
+  title = 'Micro-práctica 1';
+private productos:Array<[string,number]>=[['Arroz tres delicias',3.50],['Cerdo agridulce',5.50],['Ternera con bambu y setas',7.00]];
+public codProducto=-1;
+public lineas:Array<[string,number]>=[];
+public importe:number=0.0;
+
+public addLinea()
+ {
+ var producto=this.productos[this.codProducto][0];
+ var precio:number=this.productos[this.codProducto][1];
+ this.lineas.push([producto,precio]);
+ this.importe=this.importe+precio;
+ }
+ public delLinea(i)
+ {
+ this.importe-=this.lineas[i][1];
+ this.lineas.splice(i,1);
+ }
 }
